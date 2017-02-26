f136857e4af79844252d64ad0b628bd9c72c05dd
Fran Iglesias <franiglesias@mac.com>
2017-02-26 12:39:49 +0100

    Changes to make application autocontent

--- webroot/index.php
+++ webroot/index.php
@@ -36,8 +36,7 @@
  *
  */
     if (!defined('ROOT')) {
-        //define('ROOT', dirname(dirname(dirname(__FILE__))));
-        define('ROOT', '/Library/WebServer/Documents');
+        define('ROOT', dirname(dirname(dirname(__FILE__))));
     }
 /*
  * The actual directory name for the "app".
@@ -52,9 +51,9 @@
  *
  */
     if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-        //define('CAKE_CORE_INCLUDE_PATH', ROOT);
+        define('CAKE_CORE_INCLUDE_PATH', ROOT.DS.APP_DIR.'/vendors/cakephp');
         //define('CAKE_CORE_INCLUDE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . DS . 'libs' . DS . 'cake13');
-        define('CAKE_CORE_INCLUDE_PATH', '/Library/WebServer/lib/Cake13');
+        // define('CAKE_CORE_INCLUDE_PATH', '/Library/WebServer/lib/Cake13');
     }
 
 /*
@@ -68,6 +67,7 @@
     if (!defined('WWW_ROOT')) {
         define('WWW_ROOT', dirname(__FILE__).DS);
     }
+
     if (!defined('CORE_PATH')) {
         if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH.PATH_SEPARATOR.ROOT.DS.APP_DIR.DS.PATH_SEPARATOR.ini_get('include_path'))) {
             define('APP_PATH', null);
