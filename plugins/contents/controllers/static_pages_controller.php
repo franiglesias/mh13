b86770b3cd07fe10e3bf56dcbb9522efd9fd8b49
franiglesias <franiglesias@mac.com>
2017-03-03 10:46:27 +0100

    StaticPages and developing some macros to build headers

--- plugins/contents/controllers/static_pages_controller.php
+++ plugins/contents/controllers/static_pages_controller.php
@@ -1,108 +1,110 @@
-<?php
-class StaticPagesController extends ContentsAppController {
+<asdfasdf></asdfasdf><asdfasdf></asdfasdf><?php
 
-	var $name = 'StaticPages';
-	var $layout = 'backend';
-	var $components = array('Filters.SimpleFilters');
-	var $helpers = array('Ui.Mptt', 'Contents.StaticPage', 'Ui.Images', 'Ui.Image');
-	
-	function beforeFilter() {
-		parent::beforeFilter();
-		$this->Auth->allow(array('view','images','home'));
-	}
+class StaticPagesController extends ContentsAppController
+{
+    public $name = 'StaticPages';
+    public $layout = 'backend';
+    public $components = array('Filters.SimpleFilters');
+    public $helpers = array('Ui.Mptt', 'Contents.StaticPage', 'Ui.Images', 'Ui.Image');
 
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+        $this->Auth->allow(array('view', 'images', 'home'));
+    }
 
-	function index() {
-		$this->StaticPage->recursive = 0;
-		$this->set('staticPages', $this->paginate());
-		$this->set('allPages', $this->StaticPage->findAllParents());
-		$this->passLabelsToView();
-	}
+    public function index()
+    {
+        $this->StaticPage->recursive = 0;
+        $this->set('staticPages', $this->paginate());
+        $this->set('allPages', $this->StaticPage->findAllParents());
+        $this->passLabelsToView();
+    }
 
-	function view($slug = null) {
-		$this->layout = 'static';
-		if (!$slug) {
-			$this->Session->setFlash(sprintf(__('Invalid %s.', true), __d('contents', 'site', true)), 'flash_error');
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->StaticPage->view($slug);
-		$this->set('staticPage', $this->StaticPage->data);
-	}
-	
-	function home($slug = null) {
-		$this->setAction('view', $slug);
-	}
+    public function view($slug = null)
+    {
+        $this->layout = 'static';
+        if (!$slug) {
+            $this->Session->setFlash(sprintf(__('Invalid %s.', true), __d('contents', 'site', true)), 'flash_error');
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->StaticPage->view($slug);
+        // $this->set('staticPage', $this->StaticPage->data);
+        $this->autoRender = false;
+        echo $this->twig->render('plugins/contents/static_pages/view.twig', ['staticPage' => $this->StaticPage->data]);
+    }
 
-	function add() {
-		$data = array(
-			'title' => __d('contents', 'New page', true), 
-			'content' => __d('contents', 'Content for the page', true)
-		);
-		$this->StaticPage->create();
-		$this->StaticPage->save($data);
-		$this->setAction('edit', $this->StaticPage->id);
-	}
+    public function home($slug = null)
+    {
+        $this->setAction('view', $slug);
+    }
 
-	
-	public function edit($id = null) 
-	{
-		// Second pass
-		if (!empty($this->data['StaticPage'])) {
-			// Create model if there is no id (add action)
-			if (!$id) {
-				$this->StaticPage->create();
-			}
-			// Data massaging if it is not doable in create or beforeSave
-			
-			// Try to save
-			if ($this->StaticPage->save($this->data)) {
-				$this->message('success');
-				$this->xredirect();
-				// Force reload
-				$this->refreshModel($id);
-			} else {
-				$this->message('validation');
-			}
-		}
-		
-		// First pass or reload
-		if(empty($this->data['StaticPage'])) { // 1st pass
-			if ($id) {
-				$this->refreshModel($id);
-			}
-			$this->saveReferer(); // Store actual referer to use in 2nd pass
-		}
-		// Render preparation
-		// Create lists for options, etc.
-		$this->_useAutosave();
-		$staticParents = $this->StaticPage->candidateParents();
-		$this->passLabelsToView();
-		$this->set(compact('staticParents'));
-		
-	}
+    public function add()
+    {
+        $data = array(
+            'title' => __d('contents', 'New page', true),
+            'content' => __d('contents', 'Content for the page', true),
+        );
+        $this->StaticPage->create();
+        $this->StaticPage->save($data);
+        $this->setAction('edit', $this->StaticPage->id);
+    }
 
-	protected function refreshModel($id)
-	{
-		$this->preserveAppData();
-		// Data needed to load or reload model
-		$fields = null;
-		if (!($this->data = $this->StaticPage->read($fields, $id))) {
-			$this->message('error');
-			$this->xredirect(); // forget stored referer and redirect
-		}
-		$this->restoreAppData();
-	}
-	
-	protected function passLabelsToView()
-	{
-		// App::import('Model', 'Labels.Label');
-		$this->loadModel('Labels.Label');
-		$this->set(
-			'globalLabels',
-			$this->Label->getGlobal()
-		);
-	}
-	
+    public function edit($id = null)
+    {
+        // Second pass
+        if (!empty($this->data['StaticPage'])) {
+            // Create model if there is no id (add action)
+            if (!$id) {
+                $this->StaticPage->create();
+            }
+            // Data massaging if it is not doable in create or beforeSave
 
+            // Try to save
+            if ($this->StaticPage->save($this->data)) {
+                $this->message('success');
+                $this->xredirect();
+                // Force reload
+                $this->refreshModel($id);
+            } else {
+                $this->message('validation');
+            }
+        }
+
+        // First pass or reload
+        if (empty($this->data['StaticPage'])) { // 1st pass
+            if ($id) {
+                $this->refreshModel($id);
+            }
+            $this->saveReferer(); // Store actual referer to use in 2nd pass
+        }
+        // Render preparation
+        // Create lists for options, etc.
+        $this->_useAutosave();
+        $staticParents = $this->StaticPage->candidateParents();
+        $this->passLabelsToView();
+        $this->set(compact('staticParents'));
+    }
+
+    protected function refreshModel($id)
+    {
+        $this->preserveAppData();
+        // Data needed to load or reload model
+        $fields = null;
+        if (!($this->data = $this->StaticPage->read($fields, $id))) {
+            $this->message('error');
+            $this->xredirect(); // forget stored referer and redirect
+        }
+        $this->restoreAppData();
+    }
+
+    protected function passLabelsToView()
+    {
+        // App::import('Model', 'Labels.Label');
+        $this->loadModel('Labels.Label');
+        $this->set(
+            'globalLabels',
+            $this->Label->getGlobal()
+        );
+    }
 }
-?>
--- plugins/contents/views/elements/items/widgets/downloads.ctp
+++
@@ -1,8 +0,0 @@
-<aside class="mh-page-extras" id="mh-downloads">
-	<div class="mh-page-extras-container">
-	<?php if ((!empty($item['Enclosure']) && $item['Enclosure']['type'] === 'application/pdf') || count($item['Download'])): ?>
-		<h2 class="mh-page-extras-title"><?php __d('contents', 'Download'); ?></h2>
-		<?php echo $this->Media->downloads($item['Download']); ?>
-	<?php endif ?>
-	</div>
-</aside>
