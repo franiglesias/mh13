e46866a6516468fa890722e41beeadbf2caf090a
Fran Iglesias <franiglesias@mac.com>
2017-03-21 21:18:21 +0100

    Starting to try Silex

--- webroot/prueba.php
+++ webroot/prueba.php
@@ -1,1 +1,50 @@
-<?php echo  phpinfo(); ?>
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->register(
+    new Silex\Provider\TwigServiceProvider(),
+    [
+        'twig.path' => __DIR__.'/../views',
+        'twig.options' => [
+            'auto_reload' => true,
+            'cache' => __DIR__.'/../tmp/cache/twig2',
+        ],
+    ]
+);
+
+require_once('../config/mh13.php');
+
+$app->extend(
+    'twig',
+    function ($twig, $app) use ($config) {
+        foreach ($config as $key => $value) {
+            $twig->addGlobal($key, $config[$key]);
+        }
+        $twig->addGlobal('title_for_layout', 'Página principal');
+        $twig->addGlobal('BaseUrl', '/');
+
+        return $twig;
+    }
+);
+
+$app->get(
+    '/hello/{name}',
+    function ($name) use ($app) {
+        return 'Hello '.$app->escape($name);
+    }
+);
+
+$app->get(
+    '/',
+    function () use ($app) {
+        return $app['twig']->render('pages/index.twig');
+    }
+);
+
+$app->run();
